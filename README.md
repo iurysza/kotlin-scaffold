632e32bf764360be904c429fa7a40ddd71e2b6a8
Iury Souza <iury.souza@ifood.com.br>
2020-11-07 19:00:14 -0300

    init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Kotlin Scaffold
+
+This is a kotlin project setup using some gradle plugins to enable fast and reliable library development
+
+## Spotless
+- Strict code styling applied to every build
+- IDE formatter integration
+
+
+## Pre-commit hook
+- git hook auto installer
+
+## ktlint and detekt tasks
+- Runs ktlint and detekt over files that changed on your commit.
+
+## Release & Publish
+- Provides a simple way to upload the library to a nexus repository
+- Create tasks to generate version tags
---
+++ src/test/kotlin/com/iurysza/kotlinscaffold/LibraryTest.kt
@@ -0,0 +1,30 @@
+
+/*
+ * Copyright (C) 2020 Iury Souza.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.iurysza.kotlinscaffold
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+
+class LibraryTest : BehaviorSpec({
+  given("an instance of Library") {
+    `when`("some library method is called") {
+      then("it should return true") {
+        Library().someLibraryMethod() shouldBe true
+      }
+    }
+  }
+})
